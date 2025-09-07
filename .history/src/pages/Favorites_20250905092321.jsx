1df280bc7183995d6a262bdc2e5ca1c97fff611e
HarryPotter1317 <darjeeprachi5@gmail.com>
2025-09-07 16:35:44 +0530

    Adding merged-code on 7-9-25

--- .history/src/pages/Favorites_20250905092321.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-export default function Favorites(){
-  return (
-    <div style={{padding:20, color:'#e6eef8'}}>
-      <h2>Your Favorites</h2>
-      <p>You can add favorite recipes here (future upgrade).</p>
-    </div>
-  )
-}
